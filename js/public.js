98058d17d63eabc774c95c01de8667e878eddf1a
XueGi0 <15535955420@163.com>
2018-07-23 22:01:02 +0800

    测试摄像头

---
+++ js/public.js
@@ -0,0 +1,13 @@
+~function () {
+  let computed = function () {
+    let desW = 375;
+    let devW = document.documentElement.clientWidth;
+    if (devW >= 375) {
+      document.documentElement.style.fontSize = '100px';
+      return;
+    }
+    document.documentElement.style.fontSize = devW / desW * 100 + 'px';
+  };
+  computed();
+  window.addEventListener('resize', computed, false);
+}();
