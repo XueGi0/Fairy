8da6c027a4aa776b107bc38842cfdbeafca9a6c7
XueGi0 <15535955420@163.com>
2018-07-27 12:04:47 +0800

    修改弹出框样式

--- js/showTips.js
+++ js/showTips.js
@@ -2,13 +2,13 @@ function showTips(msg, state) {
   let htmlCon = '';
   if (msg != '') {
     if (state == '0') {
-      htmlCon = "<section class='tipsBox' style='width: 60%;margin-top:-20%;height: 20%;background: #000;position: absolute;top: 50%;left: 50%;margin-left: -30%;border-radius: 3%;opacity: .3;color: #fff;font-size: .14rem;text-align: center;padding: 0 5%'><i class='msgIcon sucIcon'></i>" + msg + "</section>";
+      htmlCon = "<section class='tipsBox' style='width: 60%;margin-top:-20%;height: 20%;background: #000;position: absolute;top: 50%;left: 50%;margin-left: -30%;border-radius:.2rem;opacity: .3;color: #fff;font-size: .15rem;text-align: center'><i class='msgIcon sucIcon'></i>" + msg + "</section>";
     } else {
-      htmlCon = "<section class='tipsBox' style='width: 60%;margin-top:-20%;height: 20%;background: #000;position: absolute;top: 50%;left: 50%;margin-left: -30%;border-radius: 3%;opacity: .3;color: #fff;font-size: .14rem;text-align: center;padding: 0 5%'><i class='msgIcon failIcon'></i>" + msg + "</section>";
+      htmlCon = "<section class='tipsBox' style='width: 60%;margin-top:-20%;height: 20%;background: #000;position: absolute;top: 50%;left: 50%;margin-left: -30%;border-radius:.2rem;opacity: .3;color: #fff;font-size: .15rem;text-align: center'><i class='msgIcon failIcon'></i>" + msg + "</section>";
     }
   }
   $('body').prepend(htmlCon);
   setTimeout(function () {
     $('.tipsBox').remove();
-  }, 1000);
+  }, 1500);
 }
